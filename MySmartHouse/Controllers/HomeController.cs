1090f79c7a353e2e6e8cd2c566496d673a0a3478
Денис Зубаиров <kosmoden4ik@gmail.com>
2019-04-12 21:40:32 +0300

    Project

--- MySmartHouse/Controllers/HomeController.cs
+++ MySmartHouse/Controllers/HomeController.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿
 using System.Web.Mvc;
 
 namespace MySmartHouse.Controllers
@@ -10,6 +7,7 @@ namespace MySmartHouse.Controllers
     {
         public ActionResult Index()
         {
+               
             return View();
         }
 
